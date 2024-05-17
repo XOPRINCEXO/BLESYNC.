d54a9d3d78d7df7f28723092cb5b0e67ef43bcfd
Prince <princesindhu4004@gmail.com>
2024-05-18 04:28:21 +0530

    Initial Commit

---
+++ BLESYNC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLESYNC
+//
+//  Created by Prince on 18/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
